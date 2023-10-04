12688a74328bbd422e8e89839a4464925a8e11f6
Alexander Symanzik <alexander-symanzik@web.de>
2023-10-04 19:21:14 +0200

    [TASK] Finishes base tutorial of dynamic routes

--- nextjs-blog/lib/posts.js
+++ nextjs-blog/lib/posts.js
@@ -44,3 +44,52 @@ export function getSortedPostsData() {
 		}
 	});
 }
+
+/** 
+ * @description Return a list of file names, excluding .md 
+ * 
+ * @return array : objects
+ * */
+export function getAllPostIds() {
+	const fileNames = fs.readdirSync(postsDirectory);
+
+	// Returns an array that looks like this:
+	// [
+	//   {
+	//     params: {
+	//       id: 'ssg-ssr'
+	//     }
+	//   },
+	//   {
+	//     params: {
+	//       id: 'pre-rendering'
+	//     }
+	//   }
+	// ]
+	return fileNames.map((fileName) => {
+		return {
+			params: {
+				id: fileName.replace(/\.md$/, ''),
+			},
+		};
+	});
+}
+
+/**
+ * @description Returns the post data based on id
+ * @param {*} id 
+ * @returns 
+ */
+export function getPostData(id) {
+	const fullPath = path.join(postsDirectory, `${id}.md`);
+	const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+	// Use gray-matter to parse the post metadata section
+	const matterResult = matter(fileContents);
+
+	// Combine the data with the id
+	return {
+		id,
+		...matterResult.data,
+	};
+}
---
+++ nextjs-blog/pages/posts/[id].js
@@ -0,0 +1,52 @@
+// JS-Functionality
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+// import Layouts to render site
+import Layout from '../../components/layout';
+
+// Adds styling
+import styles from '../../styles/Home.module.css';
+
+/**
+ * @description uses getPostData to get the post data, returns this as props
+ * @param {*} params 
+ * @returns props
+ */
+export async function getStaticProps({ params }) {
+
+	// How does params.id from getStaticProps({ params }) know the key is named id?
+	// The value from the file name.
+	const postData = getPostData(params.id);
+	return {
+		props: {
+			postData,
+		},
+	};
+}
+
+
+/**
+ * @description Get a list of possible values for id and return this list
+ * 
+ * @param
+ * @returns 
+ */
+export async function getStaticPaths() {
+	const paths = getAllPostIds(); // contains the array of known paths returned by
+	return {
+		paths,
+		fallback: false,
+	};
+}
+
+export default function Post({ postData }) {
+	return (
+		<Layout>
+			{postData.title}
+			<br />
+			{postData.id}
+			<br />
+			{postData.date}
+		</Layout>
+	);
+}
